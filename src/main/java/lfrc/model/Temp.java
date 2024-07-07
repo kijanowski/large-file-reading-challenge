a3b231bc2ae3770797ac905a7159904c52f5ae1d
Jaroslaw Kijanowski <jaroslaw.kijanowski@redcompass.com>
2024-07-07 10:11:08 +0200

    Replace synchronized with an explicit lock

--- src/main/java/lfrc/model/Temp.java
+++ src/main/java/lfrc/model/Temp.java
@@ -1,15 +1,35 @@
 package lfrc.model;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Temp {
+
+    // enabling fairness depends on the business requirements and the amount of readings arriving
+    private final ReadWriteLock lock = new ReentrantReadWriteLock(true);
+
+    // int and float would do it for this particular scenario
     private long count = 0;
     private double total = 0.0;
 
-    public synchronized void addReading(double temperature) {
-        count++;
-        total += temperature;
+    public void addReading(double temperature) {
+        lock.writeLock().lock();
+        try {
+            // consider throwing an exception when count or total overflows
+            // very unlikely in this particular scenario, where we have only one temperature reading per day
+            count++;
+            total += temperature;
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
-    public synchronized double average() {
-        return total / count;
+    public double average() {
+        lock.readLock().lock();
+        try {
+            return total / count;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
